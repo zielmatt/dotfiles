01caf8c26f8db02922b1f1c45a8d77aa8baad252
Matthew Zielinski <mateusz.zielinski@affirm.com>
2025-06-17 14:17:39 -0700

    add installation script

---
+++ install.sh
@@ -0,0 +1,12 @@
+sym() {
+    dotdir="$1"
+
+    for filename in $(find $dotdir -name "*.symlink"); do
+      ln -sf $filename ~/.$(basename $filename | sed "s/\.symlink//")
+    done
+}
+
+dotfs=$(cd $(dirname $0) && pwd)
+
+sym "$dotfs"
+
